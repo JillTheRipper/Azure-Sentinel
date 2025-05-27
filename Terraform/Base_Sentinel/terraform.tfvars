2913d4b849d481cc95fe9f622e54c3b043720f69
JillTheRipper <134748702+JillTheRipper@users.noreply.github.com>
2025-05-26 20:25:06 -0700

    Add files via upload

---
+++ Terraform/Base_Sentinel/terraform.tfvars
@@ -0,0 +1,3 @@
+resource_group_name = "rg-security"
+workspace_name      = "law-security"
+location            = "East US"
---
+++ Terraform/Base_Sentinel/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+  default     = "East US"
+}
+
+variable "workspace_name" {
+  description = "Log Analytics Workspace Name"
+  type        = string
+}
