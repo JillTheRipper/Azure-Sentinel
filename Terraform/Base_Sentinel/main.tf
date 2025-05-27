c11ac5fca40001351525923a19fa0544e281c5ea
JillTheRipper <134748702+JillTheRipper@users.noreply.github.com>
2025-05-26 20:24:38 -0700

    Update main.tf

--- Terraform/Base_Sentinel/main.tf
+++ Terraform/Base_Sentinel/main.tf
@@ -1,1 +1,26 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
 
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Log Analytics Workspace
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = var.workspace_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+# Microsoft Sentinel (via solution deployment)
+resource "azurerm_sentinel_log_analytics_workspace_onboarding" "sentinel" {
+  workspace_id                 = azurerm_log_analytics_workspace.law.id
+  customer_managed_key_enabled = false
+}
